f08c63bc7fe9de14d635f4e91534015e0d1635db
jsjackson263 <jsjackson263@users.noreply.github.com>
2018-06-10 21:45:52 +1200

    use UriComponentBuilder to pass into the RestTemplate

--- src/main/java/info/jsjackson/springrestclientexamples/services/ApiServiceImpl.java
+++ src/main/java/info/jsjackson/springrestclientexamples/services/ApiServiceImpl.java
@@ -5,8 +5,10 @@ package info.jsjackson.springrestclientexamples.services;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import info.jsjackson.api.domain.User;
 import info.jsjackson.api.domain.UserData;
@@ -20,19 +22,27 @@ public class ApiServiceImpl implements ApiService {
 
 	private RestTemplate restTemplate;
 	
+	private final String api_url;
+	
 	/**
 	 * @param restTemplate
 	 */
-	public ApiServiceImpl(RestTemplate restTemplate) {
-		super();
+	public ApiServiceImpl(RestTemplate restTemplate, @Value("${api.url}") String api_url) {
 		this.restTemplate = restTemplate;
+		this.api_url = api_url;
 	}
 
 	
 	@Override
 	public List<User> getUsers(Integer limit) {
 
-		UserData userData = restTemplate.getForObject("http://apifaketory.com/api/user?limit=" + limit, UserData.class);
+		UriComponentsBuilder uriBuilder = UriComponentsBuilder
+				.fromUriString(api_url)
+				.queryParam("limit", limit);
+		
+		/*UserData userData = restTemplate.getForObject("http://apifaketory.com/api/user?limit=" + limit, UserData.class);*/
+		
+		UserData userData = restTemplate.getForObject(uriBuilder.toUriString(), UserData.class);
 		
 		return userData.getData();
 
